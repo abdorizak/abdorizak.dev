c991e6b4bd4616e88e4e5e4880da04ede193e86a
Abdirizak Abdalla  <cabdirizaaqyare12@gmail.com>
2023-09-16 09:51:19 +0300

    Removes unnecessary sources from the default-src and script-src directives in

--- config/next/headers.js
+++ config/next/headers.js
@@ -1,8 +1,6 @@
 /* eslint-disable max-len */
 const ContentSecurityPolicy = `
-  default-src 'self' vercel.live;
   worker-src 'self' blob:;
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live;
   child-src *.google.com *.unsplash.com *.scdn.co **.abdorizak.dev unavatar.now.sh *.unavatar.io cdn.discordapp.com *.cdninstagram.com;
   style-src 'self' 'unsafe-inline' *.googleapis.com;
   img-src *.cdninstagram.com *.gstatic.com * blob: data:;
